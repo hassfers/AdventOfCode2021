47d57ec917d519c36cda5a45c64ea0bdc6cf5981
Stefan Haßferter <stefanhassferter@icloud.com>
2021-12-03 11:35:06 +0100

    Day 2 Part Two

--- adventOfCode/src/Day_3.kt
+++ adventOfCode/src/Day_3.kt
@@ -25,25 +25,92 @@ class Day_3(override val isRunningExample: Boolean) : Day {
 		val transpose = input
 			.toList()
 			.transpose()
-			.map { it.groupBy { it } }
+			.map {
+				it.groupBy { it }
+					.mapValues {
+						it.value.size
+					}
+			}
 
 		val gammaRate = Integer.parseInt(
 			transpose.map {
-				it.maxByOrNull { it.value.size }
+				it.maxByOrNull { it.value }
 			}.map {
 				it!!.key
-			}.toCharArray().concatToString(), 2
+			}.toCharArray().concatToString(),
+			2
 		)
 
 		val epsilonRate = Integer.parseInt(
 			transpose.map {
-				it.minByOrNull { it.value.size }
+				it.minByOrNull { it.value }
 			}.map {
 				it!!.key
-			}.toCharArray().concatToString(), 2
+			}.toCharArray().concatToString(),
+			2
 		)
-
-
 		return "$gammaRate, $epsilonRate, ${epsilonRate * gammaRate}"
+	}
+
+	override fun solvePartTwo(input: Array<String>): String {
+		var column = 0
+		var bufferOxygen = input.toMutableList()
+		while (bufferOxygen.size > 1){
+			val columnEntry = bufferOxygen.transpose()
+				.map {
+				it.groupBy { it }
+					.mapValues {
+						it.value.size
+					}
+			}
+				.get(column)
+
+			val test = if((columnEntry.get('0')!! compareTo columnEntry.get('1')!!) == 1){
+				'0'
+			}else{
+				'1'
+			}
+
+			bufferOxygen =
+				bufferOxygen.filter {
+					it[column] == test
+				}.toMutableList()
+
+			column += 1
+			println(test)
+		}
+
+		println("oxygen $bufferOxygen, ${Integer.parseInt(bufferOxygen.first(),2)}")
+
+	column = 0
+		var co2rating = input.toMutableList()
+		while (co2rating.size > 1){
+			val columnEntry = co2rating.transpose()
+				.map {
+					it.groupBy { it }
+						.mapValues {
+							it.value.size
+						}
+				}
+				.get(column)
+
+			val test = if((columnEntry.get('1')!! compareTo columnEntry.get('0')!!) == -1){
+				'1'
+			}else{
+				'0'
+			}
+
+			co2rating =
+				co2rating.filter {
+					it[column] == test
+				}.toMutableList()
+
+			column += 1
+			println(test)
+		}
+		println(co2rating)
+		println("oxygen $co2rating, ${Integer.parseInt(co2rating.first(),2)}")
+
+		return (" ${Integer.parseInt(co2rating.first(),2) * Integer.parseInt(bufferOxygen.first(),2)}")
 	}
 }
