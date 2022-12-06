b79d2a8947857d3f739c5dab64c6c1c6c7c851af
Stefan Haßferter <hassfers@gmail.com>
2022-12-06 21:30:25 +0100

    add result

--- AOC2022/src/Day_5.kt
+++ AOC2022/src/Day_5.kt
@@ -13,6 +13,11 @@ class Day_5 : Day {
 	override val isRunningExample: Boolean
 		get() = false
 
+	var resultPartOne:String = ""
+	var resultPartTwo:String = ""
+
+
+
 	override fun solvePartOne(input: List<String>): String {
 		val startingPoint = if (isRunningExample) mutableListOf(
 			mutableListOf(
@@ -65,20 +70,87 @@ class Day_5 : Day {
 
 		operations.forEach { operations ->
 			val containersToMove = startingPoint[operations[1] - 1].takeLast(operations[0])
-			println(startingPoint)
-			println(operations.joinToString())
-			println(containersToMove)
+//			println(startingPoint)
+//			println(operations.joinToString())
+//			println(containersToMove)
 			val new = startingPoint[operations[1] - 1].dropLast(operations[0]).toMutableList()
-			println(new)
+//			println(new)
 			startingPoint[operations[1] - 1] = new
 			startingPoint[operations[2] - 1].addAll(containersToMove.reversed())
-			println(startingPoint)
-			println("----")
+//			println(startingPoint)
+//			println("----")
 		}
 
-
-		return startingPoint.map { it.last() }.toConnectedString()
+		resultPartOne = startingPoint.map { it.last() }.toConnectedString()
+		return resultPartOne
 	}
 
+	override fun solvePartTwo(input: List<String>): String {
+		val startingPoint = if (isRunningExample) mutableListOf(
+			mutableListOf(
+				"Z", "N"
+			),
+			mutableListOf(
+				"M", "C", "D"
+			),
+			mutableListOf(
+				"P"
+			)
+		) else mutableListOf(
+			mutableListOf("T", "Z", "B").asReversed(),
+			mutableListOf("N", "D", "T", "H", "V").asReversed(),
+			mutableListOf("D", "M", "F", "B").asReversed(),
+			mutableListOf("L", "Q", "V", "W", "G", "J", "T").asReversed(),
+			mutableListOf("M", "Q", "F", "V", "P", "G", "D", "W").asReversed(),
+			mutableListOf("S", "F", "H", "G", "Q", "Z", "V").asReversed(),
+			mutableListOf("W", "C", "T", "L", "R", "N", "S", "Z").asReversed(),
+			mutableListOf("M", "R", "N", "J", "D", "W", "H", "Z").asReversed(),
+			mutableListOf("S", "D", "F", "L", "Q", "M").asReversed(),
+		)
+
+
+//                                [M]     [W] [M]
+//                            [L] [Q] [S] [C] [R]
+//                            [Q] [F] [F] [T] [N] [S]
+//                    [N]     [V] [V] [H] [L] [J] [D]
+//                    [D] [D] [W] [P] [G] [R] [D] [F]
+//                [T] [T] [M] [G] [G] [Q] [N] [W] [L]
+//                [Z] [H] [F] [J] [D] [Z] [S] [H] [Q]
+//                [B] [V] [B] [T] [W] [V] [Z] [Z] [M]
+
+
+		val operations = input
+			.map { it.split("from ") }
+			.map {
+				it.flatMap {
+					it.split(" to ")
+				}
+			}
+			.map {
+				it.map {
+					it.replace("move ", "")
+				}
+					.map {
+						it.trim().toInt()
+					}
+			}
+
+		operations.forEach { operations ->
+			val containersToMove = startingPoint[operations[1] - 1].takeLast(operations[0])
+//			println(startingPoint)
+//			println(operations.joinToString())
+//			println(containersToMove)
+			val new = startingPoint[operations[1] - 1].dropLast(operations[0]).toMutableList()
+//			println(new)
+			startingPoint[operations[1] - 1] = new
+			startingPoint[operations[2] - 1].addAll(containersToMove)
+//			println(startingPoint)
+//			println("----")
+		}
+
+		resultPartTwo = startingPoint.map { it.last() }.toConnectedString()
+
+		return resultPartTwo
+	}
 
 }
--- AOC2022/src/main.kt
+++ AOC2022/src/main.kt
@@ -1,11 +1,16 @@
 package com.mypackage.AOC2022
 
+import kotlin.test.assertEquals
+
 fun main(args: Array<String>) {
 
 //Day_1().solve()
 //	Day_2().solve()
 //	Day_3().solve()
 //	Day_4().solve()
-	Day_5().solve()
+	val day = Day_5()
+	day.solve()
+	assertEquals(day.resultPartOne,"NTWZZWHFV")
+	assertEquals(day.resultPartTwo,"BRZGFVBTJ")
 }
 
